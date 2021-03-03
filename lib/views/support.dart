0fc773687476dc35996a3d7ecb4efc78ff0c3886
luka <luka@itu.dk>
2021-03-03 12:24:03 +0100

    Cleared the support page

--- lib/views/support.dart
+++ lib/views/support.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 
 class SupportPage extends StatefulWidget {
   @override
@@ -10,55 +9,80 @@ class _SupportPageState extends State<SupportPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          // Text("TODO: Implement"),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: Text("Go a page back"),
-          ),
-          ElevatedButton(
-            onPressed: () => {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => WebView(
-                          javascriptMode: JavascriptMode.unrestricted,
-                          initialUrl:
-                              'https://www.yamatomichi.com/en/support/faq/'))),
-            },
-            child: Text("FAQ"),
-          ),
-          ElevatedButton(
-            onPressed: () => {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => WebView(
-                          javascriptMode: JavascriptMode.unrestricted,
-                          initialUrl:
-                              'https://www.yamatomichi.com/en/support/contact/'))),
-            },
-            child: Text("Contact"),
-          ),
-          ElevatedButton(
-            onPressed: () => {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => WebView(
-                          javascriptMode: JavascriptMode.unrestricted,
-                          initialUrl:
-                              'https://www.yamatomichi.com/en/support/online/'))),
-            },
-            child: Text("Online Support"),
-          ),
-        ],
+      body: Center(
+        child: ListView(
+          children: <Widget>[
+            Text("1"),
+            Text("2"),
+            Text("3"),
+            Text("4"),
+            Text("5"),
+            Text("6"),
+            Text("7"),
+            Text("8"),
+            // TextField(
+            //   keyboardType: TextInputType.multiline,
+            //   minLines: 1, 
+            //   maxLines: null,
+            // ),
+          ],
+        ),
       ),
-    ));
+    );
   }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //       body: Center(
+  //     child: Column(
+  //       mainAxisAlignment: MainAxisAlignment.center,
+  //       children: <Widget>[
+  //         // Text("TODO: Implement"),
+  //         ElevatedButton(
+  //           onPressed: () {
+  //             Navigator.pop(context);
+  //           },
+  //           child: Text("Go a page back"),
+  //         ),
+  //         ElevatedButton(
+  //           onPressed: () => {
+  //             Navigator.push(
+  //                 context,
+  //                 MaterialPageRoute(
+  //                     builder: (context) => WebView(
+  //                         javascriptMode: JavascriptMode.unrestricted,
+  //                         initialUrl:
+  //                             'https://www.yamatomichi.com/en/support/faq/'))),
+  //           },
+  //           child: Text("FAQ"),
+  //         ),
+  //         ElevatedButton(
+  //           onPressed: () => {
+  //             Navigator.push(
+  //                 context,
+  //                 MaterialPageRoute(
+  //                     builder: (context) => WebView(
+  //                         javascriptMode: JavascriptMode.unrestricted,
+  //                         initialUrl:
+  //                             'https://www.yamatomichi.com/en/support/contact/'))),
+  //           },
+  //           child: Text("Contact"),
+  //         ),
+  //         ElevatedButton(
+  //           onPressed: () => {
+  //             Navigator.push(
+  //                 context,
+  //                 MaterialPageRoute(
+  //                     builder: (context) => WebView(
+  //                         javascriptMode: JavascriptMode.unrestricted,
+  //                         initialUrl:
+  //                             'https://www.yamatomichi.com/en/support/online/'))),
+  //           },
+  //           child: Text("Online Support"),
+  //         ),
+  //       ],
+  //     ),
+  //   ));
+  // }
 }
