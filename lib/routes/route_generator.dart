98f2a264d2e2563361c44a0aa2d092bd49e9b053
Julian Wulff <julianwulff@gmail.com>
2021-03-04 22:22:58 +0100

    Moved the routing with args example to docs

--- lib/routes/route_generator.dart
+++ lib/routes/route_generator.dart
@@ -9,21 +9,10 @@ import 'package:app/ui/view/unknown.dart';
 
 class RouteGenerator {
   static Route<dynamic> generateRoute(RouteSettings settings) {
-    // // //Getting arguments passed in while calling Navigator.pushNamed
-    // // final args = settings.arguments;
-
     switch (settings.name) {
       case homeRoute:
         return MaterialPageRoute(builder: (_) => HomeView());
       case signUpRoute:
-        // // // Example of passing data
-        // // // Validation of passed data - in this case a string
-        // // if(args is String) {
-        // //   return MaterialPageRoute(builder: (_) => HomeView(data: args));
-        // // }
-        // // // If args is not of the correct type, return an error page.
-        // // // Possibly throw an exceptoion while in development
-        // // return MaterialPageRoute(builder: (_) => UnknownPage());
         return MaterialPageRoute(builder: (_) => SignUpView());
       case signInRoute:
         return MaterialPageRoute(builder: (_) => SignInView());
