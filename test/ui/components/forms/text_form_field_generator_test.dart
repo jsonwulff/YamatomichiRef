cc6862ea790f1cc4a6119ff6be4a317bcab72270
luka <DoBok444>
2021-03-07 12:22:52 +0100

    Done testing text form field generator

--- test/ui/components/forms/text_form_field_generator_test.dart
+++ test/ui/components/forms/text_form_field_generator_test.dart
@@ -3,33 +3,72 @@ import 'package:app/ui/components/text_form_field_generator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+// NOTE: the widget isn't cleared after each test
 void main() {
   TextEditingController nameController = TextEditingController();
   String nameToSet = 'Satoshi Nakamoto';
   var validator = AuthenticationValidation.validateName;
   String label = 'Full Name';
   IconData icon = Icons.person;
+  String buttonText = 'Send';
 
   MaterialApp appCreator(Widget widget) {
+    final formKey = new GlobalKey<FormState>();
+
     return MaterialApp(
-        home: Scaffold(
-      body: widget,
-    ));
+      home: Scaffold(
+        body: Form(
+          key: formKey,
+          child: Column(
+            children: [
+              widget,
+              ElevatedButton(
+                onPressed: () {
+                  formKey.currentState.save();
+                },
+                child: Text(buttonText),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  MaterialApp appCreatorStandard() {
+    final formKey = new GlobalKey<FormState>();
+
+    return MaterialApp(
+      home: Scaffold(
+        body: Form(
+          key: formKey,
+          child: Column(
+            children: [
+              TextInputFormFieldComponent(
+                nameController,
+                validator,
+                label,
+                iconData: icon,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  formKey.currentState.save();
+                  formKey.currentState.validate();
+                  formKey.currentState.save();
+                },
+                child: Text(buttonText),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 
   testWidgets(
       'Create text input form field with labelText: Full Name, iconData: Icons.person',
       (WidgetTester tester) async {
-    await tester.pumpWidget(
-      appCreator(
-        TextInputFormFieldComponent(
-          nameController,
-          validator,
-          label,
-          iconData: icon,
-        ),
-      ),
-    );
+    await tester.pumpWidget(appCreatorStandard());
 
     final labelTextFinder = find.text(label);
     final iconDataFinder = find.byIcon(icon);
@@ -38,27 +77,39 @@ void main() {
     expect(iconDataFinder, findsOneWidget);
   });
 
+  testWidgets('Given an empty input on name shows error code',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(
+      appCreatorStandard(),
+    );
+
+    await tester.tap(find.byType(ElevatedButton));
+
+    await tester.pumpAndSettle();
+
+    // final submitButton = find.text(buttonText);
+    // final nameErrorMessageFinder = find.text('Please enter your name');
+
+    expect(find.text('Please enter your name'), findsOneWidget);
+    expect(nameController.text, '');
+  });
+
   testWidgets(
       'Given input text of Satoshi Nakamoto: no error is shown on text on validate, name being set to Satoshi Nakamoto',
       (WidgetTester tester) async {
     await tester.pumpWidget(
-      appCreator(
-        TextInputFormFieldComponent(
-          nameController,
-          validator,
-          label,
-          iconData: icon,
-        ),
-      ),
+      appCreatorStandard(),
     );
 
     await tester.enterText(find.byType(TextFormField), nameToSet);
 
-    final noNameEnteredFinder = find.text('Please enter your name');
+    // final submitButton = find.text(buttonText);
+    // final noNameEnteredFinder = find.text('Please enter your name');
 
-    // TODO: find a better way of checking this
-    expect(noNameEnteredFinder.toString(),
-        'zero widgets with text "Please enter your name" (ignoring offstage widgets)');
+    await tester.tap(find.byType(ElevatedButton));
+    await tester.pump();
+
+    expect(find.text('Please enter your name'), findsNothing);
     expect(nameController.text, nameToSet);
   });
 }
