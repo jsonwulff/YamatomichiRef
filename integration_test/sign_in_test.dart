b6ddb2a4f340cea1729f95fbda46f7cd4067de78
luka <DoBok444>
2021-03-15 17:06:12 +0100

    Added temp comment

--- integration_test/sign_in_test.dart
+++ integration_test/sign_in_test.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-import 'package:app/main.dart' as app;
+import 'package:app/main.dart' as app; // Is actually lib/main.dart
+import 'integration_test_helpers.dart'; 
 
-import 'integration_test_helpers.dart'; // Is actually lib/main.dart
-
+// To run test: flutter drive --driver=test_driver/integration_test.dart --target=integration_test/sign_in_test.dart
 void main() {
   final _emailTest = 'lukas98@live.dk';
   final _passwordTest = 'test1234';
