d27b9b14c856a022c1b66c662d746cd4d5d838b2
luka <DoBok444>
2021-03-17 10:17:46 +0100

    Added a not verified page

--- lib/ui/view/auth/await_verified_email.dart
+++ lib/ui/view/auth/await_verified_email.dart
@@ -1,3 +1,6 @@
+import 'package:app/middleware/firebase/email_verification.dart';
+import 'package:app/ui/helpers/open_app.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart'; // Use localization
 
@@ -9,19 +12,34 @@ class AwaitVerifiedEmailView extends StatefulWidget {
 class _AwaitVerifiedEmailViewState extends State<AwaitVerifiedEmailView> {
   @override
   Widget build(BuildContext context) {
+    final EmailVerification _emailVerification =
+        EmailVerification(FirebaseAuth.instance);
+    var user = FirebaseAuth.instance.currentUser;
     var texts = AppLocalizations.of(context);
 
     return Scaffold(
-      body: Center(child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(texts.emailNotVerified),
-          Text('mail'),
-          // ElevatedButton(onPressed: () {
-
-          // }, child: Text('Open Email'))
-        ],
-      ),),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(texts.emailNotVerified + ': ${user.email}'),
+            Text(texts.resendVerificationEmail),
+            ElevatedButton(
+              onPressed: () {
+                _emailVerification.sendVerificationEmail(user: user);
+              },
+              child: Text(texts.resendEmailButton),
+            ),
+            Text(texts.openEmailApp),
+            ElevatedButton(
+              onPressed: () {
+                OpenApp.openEmailAppViaPlatform(context, AppLocalizations.of(context));
+              },
+              child: Text(texts.openEmailAppButton),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
