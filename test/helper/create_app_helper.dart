82d6def5e905a98ddcd074b19e73f14c49ed7afc
luka <DoBok444>
2021-03-17 08:09:14 +0100

    Fixed failed tests to support view

--- test/helper/create_app_helper.dart
+++ test/helper/create_app_helper.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 class CreateAppHelper {
-  static MaterialApp generateBasicApp(Widget widget)
-  {
+  static MaterialApp generateYamatomichiTestApp(Widget widget) {
     return MaterialApp(
+      title: 'Yamatomichi',
       home: Scaffold(
         body: widget,
       ),
+      onGenerateTitle: (BuildContext context) =>
+          AppLocalizations.of(context).appTitle,
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en', ''), // English, no country code
+        const Locale('da', 'DK'), // Danish
+        const Locale('ja', '') // Japanese, for all regions
+      ],
     );
   }
 }
