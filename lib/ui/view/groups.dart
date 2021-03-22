7ca0d6ddddb940b652540e49c0c2eaf9a51da5ec
gujo <gujo@itu.dk>
2021-03-22 23:56:11 +0100

    GearView is on its way - but should be fine for demo on Wednesday

--- lib/ui/view/groups.dart
+++ lib/ui/view/groups.dart
@@ -1,7 +1,13 @@
 import 'package:app/routes/routes.dart';
-import 'package:app/ui/components/global/bottomNavBar.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart'; // Use localization
+import 'package:flutter_gen/gen_l10n/app_localizations.dart'; 
+
+
+
+// TODO : Localizations for gear reviews, packlists, hacks
+// TODO : extract a method that returns an Expanded widget instead of this redundant piece of garbage
+// TODO : set the overflow of the Textwidgets, to make sure it fits when translating  
+
 
 class GroupsView extends StatefulWidget {
   @override
@@ -34,22 +40,14 @@ class _GroupsViewState extends State<GroupsView> {
                   color: Colors.blueGrey,
                   child: Row(
                     mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    mainAxisAlignment: MainAxisAlignment.start,
                     children: [
                       Container(
-                          child: Center(
-                              child:
-                                  Icon(Icons.backpack_outlined, size: 100.0))),
+                        margin: EdgeInsets.only(left: 25.0),
+                        child: Icon(Icons.backpack_outlined, size: 100.0)),
                       Container(
-                          color: Colors.blue,
-                          margin: EdgeInsets.all(10.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text('GEAR REVIEWS', textScaleFactor: 2.0, textAlign: TextAlign.left,),
-                            ],
-                          )),
+                        margin: EdgeInsets.only(left: 10.0),
+                        child: Text('GEAR REVIEWS', textScaleFactor: 2.0, textAlign: TextAlign.left)),
                     ],
                   ),
                 ),
@@ -65,21 +63,14 @@ class _GroupsViewState extends State<GroupsView> {
                   color: Colors.blueGrey,
                   child: Row(
                     mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    mainAxisAlignment: MainAxisAlignment.start,
                     children: [
                       Container(
-                          child: Center(
-                              child:
-                                  Icon(Icons.backpack_outlined, size: 100.0))),
+                        margin: EdgeInsets.only(left: 25.0),
+                        child: Icon(Icons.backpack_outlined, size: 100.0)),
                       Container(
-                          margin: EdgeInsets.all(10.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text('PACKLISTS', textScaleFactor: 2.0, textAlign: TextAlign.left),
-                            ],
-                          )),
+                        margin: EdgeInsets.only(left: 10.0),
+                        child: Text('PACKLISTS', textScaleFactor: 2.0, textAlign: TextAlign.left)),
                     ],
                   ),
                 ),
@@ -95,17 +86,14 @@ class _GroupsViewState extends State<GroupsView> {
                   color: Colors.blueGrey,
                   child: Row(
                     mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    mainAxisAlignment: MainAxisAlignment.start,
                     children: [
                       Container(
-                        color: Colors.blue,
-                          child: Center(
-                              child:
-                                  Icon(Icons.backpack_outlined, size: 100.0))),
+                        margin: EdgeInsets.only(left: 25.0),
+                        child: Icon(Icons.backpack_outlined, size: 100.0)),
                       Container(
-                        color: Colors.blue,
-                          margin: EdgeInsets.all(10.0),
-                          child: Text('HACKS', textScaleFactor: 2.0, textAlign: TextAlign.left)),
+                        margin: EdgeInsets.only(left: 10.0),
+                        child: Text('HACKS', textScaleFactor: 2.0, textAlign: TextAlign.left)),
                     ],
                   ),
                 ),
