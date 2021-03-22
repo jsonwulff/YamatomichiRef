2c3509d34e5ce441aea71793d393169e22bd0e70
luka <DoBok444>
2021-03-22 13:39:17 +0100

    Finisehd up

--- test/ui/view/await_verified_email_dialog_test.dart
+++ test/ui/view/await_verified_email_dialog_test.dart
@@ -1,9 +1,12 @@
+import 'package:app/middleware/firebase/authentication_service_firebase.dart';
+import 'package:app/notifiers/user_profile_notifier.dart';
 import 'package:app/ui/view/auth/await_verified_email_dialog.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:provider/provider.dart';
 import '../../middleware/firebase/setup_firebase_auth_mock.dart';
 import 'package:app/routes/route_generator.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -13,25 +16,37 @@ class UserMock extends Mock implements User {}
 
 main() {
   setupFirebaseAuthMocks();
-  
+
   setUpAll(() async {
     await Firebase.initializeApp();
   });
 
-  MaterialApp appCreator({User user}) {
-    return MaterialApp(
-      home: Builder(
-        builder: (BuildContext context) {
-          return Center(
-            child: ElevatedButton(onPressed: () async {
-              await generateNonVerifiedEmailAlert(context, user: user);
-            },
-            child: Text('Press Here'),
-            ),
-          );
-        },
-      ),
-      debugShowCheckedModeBanner: false,
+  MultiProvider appCreator({User user}) {
+    return MultiProvider(
+      providers: [
+        Provider<AuthenticationService>(
+          create: (_) => AuthenticationService(FirebaseAuth.instance),
+        ),
+        StreamProvider(
+          create: (context) =>
+              context.read<AuthenticationService>().authStateChanges,
+        ),
+        ChangeNotifierProvider(create: (context) => UserProfileNotifier()),
+      ],
+      child: MaterialApp(
+        home: Builder(
+          builder: (BuildContext context) {
+            return Center(
+              child: ElevatedButton(
+                onPressed: () async {
+                  await generateNonVerifiedEmailAlert(context, user: user);
+                },
+                child: Text('Press Here'),
+              ),
+            );
+          },
+        ),
+        debugShowCheckedModeBanner: false,
         title: 'Yamatomichi',
         onGenerateRoute: RouteGenerator.generateRoute,
         onGenerateTitle: (BuildContext context) =>
@@ -47,25 +62,29 @@ main() {
           const Locale('da', 'DK'), // Danish
           const Locale('ja', '') // Japanese, for all regions
         ],
+      ),
     );
   }
 
   final _emailTest = 'test@test.com';
-  
+
   final alertDialogFinder = find.byKey(Key('EmailNotVerifiedAlertDialog'));
-  final mailHasBeenSentTextFinder = find.byKey(Key('NotVerifiedEmail_MailHasBeenSend'));
-  final resendMailTextFinder = find.byKey(Key('NotVerifiedEmail_ResendMailText'));
-  final resendMailButtonFinder = find.byKey(Key('NotVerifiedEmail_ResendMailButton'));
-  final openMailAppTextFinder = find.byKey(Key('NotVerifiedEmail_OpenMailAppText'));
-  final openMailAppButtonFinder = find.byKey(Key('NotVerifiedEmail_OpenMailAppButton'));
+  final mailHasBeenSentTextFinder =
+      find.byKey(Key('NotVerifiedEmail_MailHasBeenSend'));
+  final resendMailTextFinder =
+      find.byKey(Key('NotVerifiedEmail_ResendMailText'));
+  final resendMailButtonFinder =
+      find.byKey(Key('NotVerifiedEmail_ResendMailButton'));
+  final openMailAppButtonFinder =
+      find.byKey(Key('NotVerifiedEmail_OpenMailAppButton'));
   final closeButtonFinder = find.byKey(Key('NotVerifiedEmail_CloseButton'));
 
-  testWidgets('Ensure that the alert is created with all necessary information', (WidgetTester tester) async {
+  testWidgets('Ensure that the alert is created with all necessary information',
+      (WidgetTester tester) async {
     UserMock userMock = UserMock();
     when(userMock.email).thenReturn(_emailTest);
 
-    await tester
-        .pumpWidget(appCreator(user: userMock));
+    await tester.pumpWidget(appCreator(user: userMock));
 
     await tester.tap(find.text('Press Here'));
     await tester.pump();
@@ -74,17 +93,17 @@ main() {
     expect(mailHasBeenSentTextFinder, findsOneWidget);
     expect(resendMailTextFinder, findsOneWidget);
     expect(resendMailButtonFinder, findsOneWidget);
-    expect(openMailAppTextFinder, findsOneWidget);
     expect(openMailAppButtonFinder, findsOneWidget);
     expect(closeButtonFinder, findsOneWidget);
   });
 
-  testWidgets('Email not verified alert can be closed by pressing the close button', (WidgetTester tester) async {
+  testWidgets(
+      'Email not verified alert can be closed by pressing the close button',
+      (WidgetTester tester) async {
     UserMock userMock = UserMock();
     when(userMock.email).thenReturn(_emailTest);
 
-    await tester
-        .pumpWidget(appCreator(user: userMock));
+    await tester.pumpWidget(appCreator(user: userMock));
 
     await tester.tap(find.text('Press Here'));
     await tester.pump();
