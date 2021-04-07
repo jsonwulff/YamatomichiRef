036dab903f51ee3a859f0e9114f641570429c7b8
Lukas Vinther Offenberg Larsen <lukas98@live.dk>
2021-04-07 08:50:38 +0200

    Merge new navigation bar with new design

--- lib/ui/shared/navigation/bottom_navbar_with_fab.dart
+++ lib/ui/shared/navigation/bottom_navbar_with_fab.dart
@@ -1,44 +1,171 @@
-import 'package:app/ui/routes/routes.dart';
-import 'package:flutter/material.dart';
-import 'package:app/ui/shared/buttons/fancy_fab.dart';
+// import 'package:app/middleware/firebase/authentication_service_firebase.dart';
+// import 'package:app/ui/routes/routes.dart';
+// import 'package:flutter/material.dart';
+// import 'package:app/ui/shared/buttons/fancy_fab.dart';
+// import 'package:flutter_gen/gen_l10n/app_localizations.dart'; // Use localization
 
-class BottomNavBarWithFAB extends StatelessWidget {
-  BottomNavBarWithFAB({Key key, List<BottomNavigationBarItem> items, this.onTap, this.icon})
-      : super(key: key);
+// class BottomNavBarWithFAB extends StatelessWidget {
+//   BottomNavBarWithFAB({Key key, List<BottomNavigationBarItem> items, this.onTap, this.icon})
+//       : super(key: key);
 
-  final String icon;
-  final dynamic onTap;
+//   final String icon;
+//   final dynamic onTap;
 
-  @override
-  Widget build(BuildContext context) {
-    return BottomAppBar(
-      color: Colors.black,
-      child: new Row(
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          IconButton(
-            icon: Icon(Icons.calendar_today, color: Colors.white),
-            onPressed: () {
-              Navigator.pushNamedAndRemoveUntil(
-                  context, calendarRoute, (Route<dynamic> route) => false);
-            },
-          ),
-          IconButton(
-            icon: Icon(Icons.group, color: Colors.white),
-            onPressed: () {
-              Navigator.pushNamed(context, groupsRoute);
-            },
-          ),
-          IconButton(
-            icon: Icon(Icons.directions_walk_outlined, color: Colors.white),
-            onPressed: () {
-              Navigator.pushNamed(context, gearRoute);
-            },
-          ),
-          FancyFab()
-        ],
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     var texts = AppLocalizations.of(context);
+
+//     return BottomNavigationBar(
+//             type: BottomNavigationBarType.fixed,
+//             backgroundColor: Colors.black,
+//             showSelectedLabels: false,
+//             showUnselectedLabels: false,
+//             items: [
+//               BottomNavigationBarItem(
+//                 icon: Icon(Icons.calendar_today, color: Colors.white),
+//                 label: texts
+//                     .calendar, // must not be null, and 'title: ..' is deprecated
+//               ),
+//               BottomNavigationBarItem(
+//                 icon: Icon(Icons.backpack_outlined, color: Colors.white),
+//                 label: texts
+//                     .gearReview, // must not be null, and 'title: ..' is deprecated
+//               ),
+//               BottomNavigationBarItem(
+//                 icon: Icon(Icons.star_border, color: Colors.white),
+//                 label: texts
+//                     .gearReview, // must not be null, and 'title: ..' is deprecated
+//               ),
+//               BottomNavigationBarItem(
+//                 label: texts.male, // TODO something else like menu
+//                 icon: IconButton(
+//                   icon: Icon(Icons.menu, color: Colors.white),
+//                   onPressed: () {
+//                     showModalBottomSheet<void>(
+//                       context: context,
+//                       shape: RoundedRectangleBorder(
+//                         borderRadius: BorderRadius.only(
+//                             topLeft: Radius.circular(15.0),
+//                             topRight: Radius.circular(15.0)),
+//                       ),
+//                       builder: (BuildContext context) {
+//                         return SafeArea(
+//                           child: Column(
+//                             crossAxisAlignment: CrossAxisAlignment.center,
+//                             mainAxisSize: MainAxisSize.min,
+//                             // height: 330,
+//                             children: <Widget>[
+//                               Divider(thickness: 1),
+//                               ListTile(
+//                                 title: Text(
+//                                   texts.profile,
+//                                   textAlign: TextAlign.center,
+//                                 ),
+//                                 // dense: true,
+//                                 onTap: () {
+//                                   Navigator.pushNamed(context, profileRoute);
+//                                 },
+//                               ),
+//                               Divider(
+//                                 thickness: 1,
+//                                 height: 5,
+//                               ),
+//                               ListTile(
+//                                 title: Text(
+//                                   texts.support,
+//                                   textAlign: TextAlign.center,
+//                                 ),
+//                                 onTap: () {
+//                                   Navigator.pushNamed(context, supportRoute);
+//                                 },
+//                               ),
+//                               /*Divider(
+//                                 thickness: 1,
+//                                 height: 5,
+//                               ),
+//                               ListTile(
+//                                 title: Text(
+//                                   texts.settings,
+//                                   textAlign: TextAlign.center,
+//                                 ),
+//                                 onTap: () {
+//                                   Navigator.pushNamed(context, supportRoute); // TODO Settings route
+//                                 },
+//                               ),*/
+//                               Divider(thickness: 1),
+//                               ListTile(
+//                                 title: Text(
+//                                   texts.signOut,
+//                                   textAlign: TextAlign.center,
+//                                 ),
+//                                 onTap: () async {
+//                                   if (await context
+//                                       .read<AuthenticationService>()
+//                                       .signOut(context)) {
+//                                     Navigator.pushNamedAndRemoveUntil(
+//                                         context,
+//                                         signInRoute,
+//                                         (Route<dynamic> route) => false);
+//                                   }
+//                                 },
+//                               ),
+//                               Divider(thickness: 1),
+//                               ListTile(
+//                                 title: Text(
+//                                   texts.close,
+//                                   textAlign: TextAlign.center,
+//                                   style: TextStyle(color: Colors.red),
+//                                 ),
+//                                 onTap: () => Navigator.pop(context),
+//                               ),
+//                             ],
+//                           ),
+//                         );
+//                       },
+//                     );
+//                   },
+//                 ),
+//               ),
+//             ],
+//           );
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+//     // BottomAppBar(
+//     //   color: Colors.black,
+//     //   child: new Row(
+//     //     mainAxisSize: MainAxisSize.max,
+//     //     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//     //     children: <Widget>[
+//     //       IconButton(
+//     //         icon: Icon(Icons.calendar_today, color: Colors.white),
+//     //         onPressed: () {
+//     //           Navigator.pushNamedAndRemoveUntil(
+//     //               context, calendarRoute, (Route<dynamic> route) => false);
+//     //         },
+//     //       ),
+//     //       IconButton(
+//     //         icon: Icon(Icons.group, color: Colors.white),
+//     //         onPressed: () {
+//     //           Navigator.pushNamed(context, groupsRoute);
+//     //         },
+//     //       ),
+//     //       IconButton(
+//     //         icon: Icon(Icons.directions_walk_outlined, color: Colors.white),
+//     //         onPressed: () {
+//     //           Navigator.pushNamed(context, gearRoute);
+//     //         },
+//     //       ),
+//     //       FancyFab()
+//     //     ],
+//     //   ),
+//     // );
+//   }
+// }
--- lib/ui/views/home.dart
+++ lib/ui/views/home.dart
@@ -1,12 +1,5 @@
 import 'package:app/middleware/api/user_profile_api.dart';
 import 'package:app/middleware/firebase/authentication_service_firebase.dart';
-import 'package:app/models/user_profile.dart';
-import 'package:app/notifiers/user_profile_notifier.dart';
-import 'package:app/routes/routes.dart';
-import 'package:app/notifiers/navigatiobar_notifier.dart';
-import 'package:app/ui/view/calendar/calendar.dart';
-import 'package:app/ui/view/packlist/packlist.dart';
-import 'package:app/ui/view/profile/profile.dart';
 import 'package:app/middleware/models/user_profile.dart';
 import 'package:app/middleware/notifiers/user_profile_notifier.dart';
 import 'package:app/ui/routes/routes.dart';
@@ -27,14 +20,14 @@ class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
 // outcomment
-    var currentTab = [
-      CalendarView(),
-      PacklistView(),
-      GearReviewView(),
-      ProfileView()
-    ]; // supportview is only temporary as the last element
+    // var currentTab = [
+    //   CalendarView(),
+    //   PacklistView(),
+    //   GearReviewView(),
+    //   ProfileView()
+    // ]; // supportview is only temporary as the last element
 
-    var provider = Provider.of<BottomNavigationBarProvider>(context);
+    // var provider = Provider.of<BottomNavigationBarProvider>(context);
     // end of outcomment
 
     var texts = AppLocalizations.of(context);
@@ -99,128 +92,128 @@ class _HomeViewState extends State<HomeView> {
       );
     } else {
 // outcomment
-      return Scaffold(
-          backgroundColor: Colors.black,
-          body: SafeArea(
-            child: currentTab[provider.currentIndex],
-          ),
-          bottomNavigationBar: BottomNavigationBar(
-            type: BottomNavigationBarType.fixed,
-            backgroundColor: Colors.black,
-            showSelectedLabels: false,
-            showUnselectedLabels: false,
-            currentIndex: provider.currentIndex,
-            onTap: (index) {
-              provider.currentIndex = index;
-            },
-            items: [
-              BottomNavigationBarItem(
-                icon: Icon(Icons.calendar_today, color: Colors.white),
-                label: texts
-                    .calendar, // must not be null, and 'title: ..' is deprecated
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.backpack_outlined, color: Colors.white),
-                label: texts
-                    .gearReview, // must not be null, and 'title: ..' is deprecated
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.star_border, color: Colors.white),
-                label: texts
-                    .gearReview, // must not be null, and 'title: ..' is deprecated
-              ),
-              BottomNavigationBarItem(
-                label: texts.male, // TODO something else like menu
-                icon: IconButton(
-                  icon: Icon(Icons.menu, color: Colors.white),
-                  onPressed: () {
-                    showModalBottomSheet<void>(
-                      context: context,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(15.0),
-                            topRight: Radius.circular(15.0)),
-                      ),
-                      builder: (BuildContext context) {
-                        return SafeArea(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            mainAxisSize: MainAxisSize.min,
-                            // height: 330,
-                            children: <Widget>[
-                              Divider(thickness: 1),
-                              ListTile(
-                                title: Text(
-                                  texts.profile,
-                                  textAlign: TextAlign.center,
-                                ),
-                                // dense: true,
-                                onTap: () {
-                                  Navigator.pushNamed(context, profileRoute);
-                                },
-                              ),
-                              Divider(
-                                thickness: 1,
-                                height: 5,
-                              ),
-                              ListTile(
-                                title: Text(
-                                  texts.support,
-                                  textAlign: TextAlign.center,
-                                ),
-                                onTap: () {
-                                  Navigator.pushNamed(context, supportRoute);
-                                },
-                              ),
-                              /*Divider(
-                                thickness: 1,
-                                height: 5,
-                              ),
-                              ListTile(
-                                title: Text(
-                                  texts.settings,
-                                  textAlign: TextAlign.center,
-                                ),
-                                onTap: () {
-                                  Navigator.pushNamed(context, supportRoute); // TODO Settings route
-                                },
-                              ),*/
-                              Divider(thickness: 1),
-                              ListTile(
-                                title: Text(
-                                  texts.signOut,
-                                  textAlign: TextAlign.center,
-                                ),
-                                onTap: () async {
-                                  if (await context
-                                      .read<AuthenticationService>()
-                                      .signOut(context)) {
-                                    Navigator.pushNamedAndRemoveUntil(
-                                        context,
-                                        signInRoute,
-                                        (Route<dynamic> route) => false);
-                                  }
-                                },
-                              ),
-                              Divider(thickness: 1),
-                              ListTile(
-                                title: Text(
-                                  texts.close,
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(color: Colors.red),
-                                ),
-                                onTap: () => Navigator.pop(context),
-                              ),
-                            ],
-                          ),
-                        );
-                      },
-                    );
-                  },
-                ),
-              ),
-            ],
-          ));
+      // return Scaffold(
+      //     backgroundColor: Colors.black,
+      //     body: SafeArea(
+      //       child: currentTab[provider.currentIndex],
+      //     ),
+      //     bottomNavigationBar: BottomNavigationBar(
+      //       type: BottomNavigationBarType.fixed,
+      //       backgroundColor: Colors.black,
+      //       showSelectedLabels: false,
+      //       showUnselectedLabels: false,
+      //       currentIndex: provider.currentIndex,
+      //       onTap: (index) {
+      //         provider.currentIndex = index;
+      //       },
+      //       items: [
+      //         BottomNavigationBarItem(
+      //           icon: Icon(Icons.calendar_today, color: Colors.white),
+      //           label: texts
+      //               .calendar, // must not be null, and 'title: ..' is deprecated
+      //         ),
+      //         BottomNavigationBarItem(
+      //           icon: Icon(Icons.backpack_outlined, color: Colors.white),
+      //           label: texts
+      //               .gearReview, // must not be null, and 'title: ..' is deprecated
+      //         ),
+      //         BottomNavigationBarItem(
+      //           icon: Icon(Icons.star_border, color: Colors.white),
+      //           label: texts
+      //               .gearReview, // must not be null, and 'title: ..' is deprecated
+      //         ),
+      //         BottomNavigationBarItem(
+      //           label: texts.male, // TODO something else like menu
+      //           icon: IconButton(
+      //             icon: Icon(Icons.menu, color: Colors.white),
+      //             onPressed: () {
+      //               showModalBottomSheet<void>(
+      //                 context: context,
+      //                 shape: RoundedRectangleBorder(
+      //                   borderRadius: BorderRadius.only(
+      //                       topLeft: Radius.circular(15.0),
+      //                       topRight: Radius.circular(15.0)),
+      //                 ),
+      //                 builder: (BuildContext context) {
+      //                   return SafeArea(
+      //                     child: Column(
+      //                       crossAxisAlignment: CrossAxisAlignment.center,
+      //                       mainAxisSize: MainAxisSize.min,
+      //                       // height: 330,
+      //                       children: <Widget>[
+      //                         Divider(thickness: 1),
+      //                         ListTile(
+      //                           title: Text(
+      //                             texts.profile,
+      //                             textAlign: TextAlign.center,
+      //                           ),
+      //                           // dense: true,
+      //                           onTap: () {
+      //                             Navigator.pushNamed(context, profileRoute);
+      //                           },
+      //                         ),
+      //                         Divider(
+      //                           thickness: 1,
+      //                           height: 5,
+      //                         ),
+      //                         ListTile(
+      //                           title: Text(
+      //                             texts.support,
+      //                             textAlign: TextAlign.center,
+      //                           ),
+      //                           onTap: () {
+      //                             Navigator.pushNamed(context, supportRoute);
+      //                           },
+      //                         ),
+      //                         /*Divider(
+      //                           thickness: 1,
+      //                           height: 5,
+      //                         ),
+      //                         ListTile(
+      //                           title: Text(
+      //                             texts.settings,
+      //                             textAlign: TextAlign.center,
+      //                           ),
+      //                           onTap: () {
+      //                             Navigator.pushNamed(context, supportRoute); // TODO Settings route
+      //                           },
+      //                         ),*/
+      //                         Divider(thickness: 1),
+      //                         ListTile(
+      //                           title: Text(
+      //                             texts.signOut,
+      //                             textAlign: TextAlign.center,
+      //                           ),
+      //                           onTap: () async {
+      //                             if (await context
+      //                                 .read<AuthenticationService>()
+      //                                 .signOut(context)) {
+      //                               Navigator.pushNamedAndRemoveUntil(
+      //                                   context,
+      //                                   signInRoute,
+      //                                   (Route<dynamic> route) => false);
+      //                             }
+      //                           },
+      //                         ),
+      //                         Divider(thickness: 1),
+      //                         ListTile(
+      //                           title: Text(
+      //                             texts.close,
+      //                             textAlign: TextAlign.center,
+      //                             style: TextStyle(color: Colors.red),
+      //                           ),
+      //                           onTap: () => Navigator.pop(context),
+      //                         ),
+      //                       ],
+      //                     ),
+      //                   );
+      //                 },
+      //               );
+      //             },
+      //           ),
+      //         ),
+      //       ],
+      //     ));
 // end of outcomment
       WidgetsBinding.instance.addPostFrameCallback(
         (_) {
