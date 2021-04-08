15b7524244b4617f72ca4eec5e155be78d91c285
Lukas Vinther Offenberg Larsen <lukas98@live.dk>
2021-04-08 17:57:58 +0200

    Fixed japanesse lorem ipsum

--- lib/assets/fonts/fonts.dart
+++ lib/assets/fonts/fonts.dart
@@ -9,7 +9,7 @@ class FontThemes {
     return TextTheme(
       headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
       headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
-      bodyText1: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+      bodyText1: TextStyle(fontSize: 14.0, fontFamily: 'Hind', color: Colors.black),
     );
   }
 }
