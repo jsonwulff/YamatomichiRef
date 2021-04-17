956df782911af0ee4c3fa1875eba29385e2994a5
Ellen-Sofie Frydenlund Krintel Christensen <elch@itu.dk>
2021-04-17 09:45:08 +0200

    appbar looks like figma in filter

--- lib/ui/views/filters/filter_for_packlist.dart
+++ lib/ui/views/filters/filter_for_packlist.dart
@@ -17,77 +17,128 @@ class _FiltersForPacklistState extends State<FiltersForPacklistView> {
     var texts = AppLocalizations.of(context);
 
     return Scaffold(
-        appBar: AppBar(
-          iconTheme:
-              IconThemeData(color: Colors.black), //change your color here
-          title: Text("Filters for packlist",
-              style: TextStyle(color: Colors.black)),
+      appBar: AppBar(
+        leadingWidth: 65,
+        leading: Container(
+          child: TextButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: Text(
+              texts.cancel,
+              style: TextStyle(color: Colors.grey),
+            ),
+          ),
         ),
-        body: ListView(
-          children: [
-            Text(texts.amountOfDays),
-            SliderTheme(
-              data:
-                  SliderThemeData(), // TODO GET TICK / CURRENT VALUE TO SHOW HERE UI
-              child: RangeSlider(
-                  values: _currentDaysValues,
-                  min: 0,
-                  max: 20,
-                  labels: RangeLabels(
-                    _currentDaysValues.start.round().toString(),
-                    _currentDaysValues.end.round().toString(),
-                  ),
-                  onChanged: (RangeValues values) {
-                    setState(() {
-                      _currentDaysValues = values;
-                    });
-                  }),
+        title: Text("Packlist filters", style: TextStyle(color: Colors.black)),
+        actions: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(right: 20.0),
+            child: TextButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/packList');
+              },
+              child: Text("Apply"),
             ),
-            Text(texts.season),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: [
-                  SelectableText("Fall",
-                      style: TextStyle(fontWeight: FontWeight.bold)),
-                  SelectableText("Winter"),
-                  SelectableText("Spring"),
-                  SelectableText("Summer")
-                ],
-              ),
+          ),
+        ],
+      ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(texts.amountOfDays),
+          ),
+          SliderTheme(
+            data:
+                SliderThemeData(), // TODO GET TICK / CURRENT VALUE TO SHOW HERE UI
+            child: RangeSlider(
+                values: _currentDaysValues,
+                min: 0,
+                max: 20,
+                labels: RangeLabels(
+                  _currentDaysValues.start.round().toString(),
+                  _currentDaysValues.end.round().toString(),
+                ),
+                onChanged: (RangeValues values) {
+                  setState(() {
+                    _currentDaysValues = values;
+                  });
+                }),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(texts.season),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                SelectableText("Fall",
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+                SelectableText("Winter"),
+                SelectableText("Spring"),
+                SelectableText("Summer")
+              ],
             ),
-            Text(texts.category),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: [
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(texts.category),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Row(children: [
                   SelectableText("Hike"),
                   SelectableText("Snow hike"),
                   SelectableText("Fastpacking"),
                   SelectableText("Ski"),
+                ]),
+                Row(children: [
                   SelectableText("Run"),
                   SelectableText("Popup"),
                   SelectableText("UL101"),
                   SelectableText("MYOG Workshop"),
                   SelectableText("Repair Workshop"),
-                ],
-              ),
+                ]),
+              ],
             ),
-            Text(texts.weight),
-            RangeSlider(
-                values: _currentWeightValues,
-                min: 0,
-                max: 20,
-                labels: RangeLabels(
-                  _currentWeightValues.start.round().toString(),
-                  _currentWeightValues.end.round().toString(),
-                ),
-                onChanged: (RangeValues values) {
-                  setState(() {
-                    _currentWeightValues = values;
-                  });
-                }),
-          ],
-        ));
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(texts.weight),
+          ),
+          RangeSlider(
+              values: _currentWeightValues,
+              min: 0,
+              max: 20,
+              labels: RangeLabels(
+                _currentWeightValues.start.round().toString(),
+                _currentWeightValues.end.round().toString(),
+              ),
+              onChanged: (RangeValues values) {
+                setState(() {
+                  _currentWeightValues = values;
+                });
+              }),
+          Divider(
+            color: Colors.grey,
+            height: 1.5,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text("Additionals"),
+          ),
+          //CheckboxListTile( TODO IMPLEMENT CHECKBOX
+          //    title: Text("Title"),
+          //    value: checkedValue,
+          //    onChanged: (newValue) {
+          //      setState(() {
+          //        checkedValue = newValue;
+          //      });
+          //    }),
+        ],
+      ),
+    );
   }
 }
