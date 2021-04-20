56651c0fe6165187c3e46dbefb9315cfa6858236
Lukas Vinther Offenberg Larsen <lukas98@live.dk>
2021-04-20 09:02:39 +0200

    Fixed text and chip size

---
+++ lib/ui/shared/formatters/datetime_formatter.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:intl/intl.dart';
+
+String formatDateTime(DateTime date) {
+  if (date == null) return "";
+  return DateFormat('dd-MM-yyyy - kk:mm').format(date);
+}
+
+String formatCalendarDateTime(
+    BuildContext context, DateTime startDate, DateTime endDate) {
+  var buffer = StringBuffer();
+
+  buffer.write(startDate.day);
+  buffer.write('.');
+  buffer.write(_getMonthWith3LettersAsString(context, startDate));
+
+  buffer.write(' - ');
+
+  buffer.write(endDate.day);
+  buffer.write('.');
+  buffer.write(_getMonthWith3LettersAsString(context, endDate));
+
+  return buffer.toString();
+}
+
+String _getMonthWith3LettersAsString(
+    BuildContext context, DateTime date) {
+  var texts = AppLocalizations.of(context);
+
+  switch (date.month) {
+    case 1:
+      return texts.jan;
+    case 2:
+      return texts.feb;
+    case 3:
+      return texts.mar;
+    case 4:
+      return texts.apr;
+    case 5:
+      return texts.may;
+    case 6:
+      return texts.jun;
+    case 7:
+      return texts.jul;
+    case 8:
+      return texts.aug;
+    case 9:
+      return texts.sep;
+    case 10:
+      return texts.okt;
+    case 11:
+      return texts.nov;
+    case 12:
+      return texts.dec;
+  }
+}
--- lib/ui/views/calendar/components/event_widget.dart
+++ lib/ui/views/calendar/components/event_widget.dart
@@ -1,8 +1,8 @@
 import 'package:app/middleware/api/event_api.dart';
 import 'package:app/middleware/notifiers/event_notifier.dart';
+import 'package:app/ui/shared/formatters/datetime_formatter.dart';
 import 'package:app/ui/views/calendar/components/material_custom.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
 // ignore: must_be_immutable
@@ -23,11 +23,6 @@ class EventWidget extends StatelessWidget {
 
   EventNotifier eventNotifier;
 
-  String formatDateTime(DateTime date) {
-    if (date == null) return "";
-    return DateFormat('dd-MM-yyyy - kk:mm').format(date);
-  }
-
   openEvent(BuildContext context) async {
     await getEvent(id, eventNotifier);
     Navigator.pushNamed(context, '/event');
@@ -37,11 +32,24 @@ class EventWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     eventNotifier = Provider.of<EventNotifier>(context, listen: false);
     var _theme = Theme.of(context);
+    var _media = MediaQuery.of(context);
+
+    var _categoryChip = Transform(
+      transform: Matrix4.identity()..scale(0.8),
+      child: Chip(
+        label: Text(
+          'Type of event',
+          style: TextStyle(color: Colors.white),
+        ),
+        // TODO color
+        backgroundColor: Colors.blue,
+      ),
+    );
 
     return TextButton(
         child: MaterialCustom.standard(
           Container(
-            width: MediaQuery.of(context).size.width * 0.9,
+            width: _media.size.width * 0.9,
             child: InkWell(
               child: Row(
                 children: [
@@ -60,42 +68,41 @@ class EventWidget extends StatelessWidget {
                         title,
                         style: _theme.textTheme.headline3,
                       ),
-                      Chip(
-                        label: Text(
-                          'Type of event',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        backgroundColor: Colors.blue,
-                      ),
+                      _categoryChip,
                       Row(
                         children: [
-                          Column(
-                            children: [
-                              Row(children: [
-                                Icon(Icons.location_on,
-                                    color: Color.fromRGBO(81, 81, 81, 1)),
-                                Text(
-                                  title,
-                                  style: _theme.textTheme.bodyText1,
-                                ),
-                              ]),
-                              Row(children: [
-                                Icon(Icons.calendar_today,
-                                    color: Color.fromRGBO(81, 81, 81, 1)),
-                                Text(
-                                  title,
-                                  style: _theme.textTheme.bodyText1,
-                                ),
-                              ]),
-                              Row(children: [
-                                Icon(Icons.person,
-                                    color: Color.fromRGBO(81, 81, 81, 1)),
-                                Text(
-                                  title,
-                                  style: _theme.textTheme.bodyText1,
-                                ),
-                              ]),
-                            ],
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(children: [
+                                  Icon(Icons.location_on,
+                                      color: Color.fromRGBO(81, 81, 81, 1)),
+                                  Text(
+                                    'Hokkaido, Japan',
+                                    style: _theme.textTheme.bodyText2,
+                                  ),
+                                ]),
+                                Row(children: [
+                                  Icon(Icons.calendar_today,
+                                      color: Color.fromRGBO(81, 81, 81, 1)),
+                                  Text(
+                                    formatCalendarDateTime(
+                                        context, startDate, endDate),
+                                    style: _theme.textTheme.bodyText2,
+                                  ),
+                                ]),
+                                Row(children: [
+                                  Icon(Icons.person,
+                                      color: Color.fromRGBO(81, 81, 81, 1)),
+                                  Text(
+                                    '20/30 participants',
+                                    style: _theme.textTheme.bodyText2,
+                                  ),
+                                ]),
+                              ],
+                            ),
                           ),
                           Align(
                             alignment: Alignment.bottomRight,
