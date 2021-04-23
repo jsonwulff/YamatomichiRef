964fed774df839768272cbb1dba6a7b7a76583d0
Laura Jensen <laura.jensen@outlook.com>
2021-04-23 12:40:46 +0200

    getDocumentRef test done

--- test/middleware/firebase/calendar_service_test.dart
+++ test/middleware/firebase/calendar_service_test.dart
@@ -248,35 +248,26 @@ main() {
     });
   });
 
-  /*group('get events', () {
-    final event1 = Event(
-        id: '1',
-        startDate: Timestamp.fromDate(DateTime(2021, 01, 02, 12, 0, 0)));
-    final event2 = Event(
-        id: '2',
-        startDate: Timestamp.fromDate(DateTime(2021, 01, 01, 13, 0, 0)));
-
-    test('getEventsByDate return the correct order of events', () async {
-      final ffMock = MockFirestoreInstance();
-      await ffMock.collection('calendarEvent').add(event1.toMap());
-      await ffMock.collection('calendarEvent').add(event2.toMap());
-      final store = ffMock.collection('calendarEvent');
-      var snaps = await ffMock.collection('calendarEvent').get();
+  group('get document ref', () {
+    test(
+        'getDocumentRef given event id returns the document reference for that event',
+        () async {
+      final event = Event(
+        startDate: Timestamp.fromDate(DateTime(2021, 01, 01, 0, 0, 0)),
+        endDate: Timestamp.fromDate(DateTime(2021, 01, 02, 0, 0, 0)),
+        deadline: Timestamp.fromDate(DateTime(2021, 01, 01, 0, 0, 0)),
+      );
+      var store = _firestore.collection('calendarEvent');
+      await store.add(event.toMap());
+      var snaps = await store.get();
       store.doc(snaps.docs.first.id).update({'id': snaps.docs.first.id});
-      store.doc(snaps.docs.last.id).update({'id': snaps.docs.last.id});
-      event1.id = snaps.docs.first.id;
-      event2.id = snaps.docs.last.id;
+      event.id = snaps.docs.first.id;
 
-      calendarService.changeSource(ffMock);
+      String actual = _calendarService.getDocumentRef(event.id);
+      DocumentReference expected =
+          _firestore.collection('calendarEvent').doc(event.id);
 
-      print(ffMock.dump());
-
-      
-
-      var actual = await calendarService
-          .getEventsByDate(DateTime(2021, 01, 01, 0, 0, 0));
-
-      expect(actual, [event2]);
+      expect(actual, expected.toString());
     });
-  });*/
+  });
 }
