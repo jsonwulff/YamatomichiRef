3ceafd7f0a770ca759c40099cfaaa847085abb8d
Esben Højris <esbh@itu.dk>
2021-04-28 14:39:32 +0200

    Refactor PackListService and PackListAPI
    Removed firebase dependency in service, mainly

--- lib/middleware/firebase/packlist_service.dart
+++ lib/middleware/firebase/packlist_service.dart
@@ -1,41 +1,53 @@
 import 'dart:async';
 import 'package:app/middleware/api/packlist_api.dart';
-import 'package:app/middleware/firebase/user_profile_service.dart';
 import 'package:app/middleware/models/packlist.dart';
 import 'package:app/middleware/notifiers/packlist_notifier.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
 class PacklistService {
-  //final FirebaseFirestore _store = FirebaseFirestore.instance;
-  CollectionReference packlists;
-  FirebaseFirestore _store = FirebaseFirestore.instance;
-  UserProfileService userProfileService = UserProfileService();
-
-  changeSource(FirebaseFirestore store) {
-    _store = store;
-  }
-
-  PacklistService() {
-    packlists = _store.collection('packlists');
-  }
+  PacklistService();
 
   Future<String> addNewPacklist(
-      Map<String, dynamic> data, PacklistNotifier packlistNotifier) async {
-    var ref = await addPacklistToFirestore(data);
-    if (ref != null) await getPacklistAPI(ref, packlistNotifier);
+      Packlist data, PacklistNotifier packlistNotifier) async {
+    String ref = await addPacklistToFirestore(data);
+    if (ref != null) {
+      data.id = ref;
+      packlistNotifier.packlist = data;
+    }
     return 'Success';
   }
 
-  Future<List<Map<String, dynamic>>> getPacklists() async {
-    var snaps = await packlists.orderBy('createdAt').get();
-    List<Map<String, dynamic>> packlistslist = [];
-    snaps.docs.forEach((element) => packlistslist.add(element.data()));
-    return packlistslist;
+  Future<List<Packlist>> getPacklists() async {
+    return await getPackListsAPI();
   }
 
-  getSnapshots() {
-    return packlists.snapshots();
+  Future<void> deletePacklist(Packlist packlist) async {
+    await deletePacklistAPI(packlist);
   }
+
+  Future<void> updatePacklist(Packlist packlist, Map<String, dynamic> map,
+      Function packlistUpdated) async {
+    await updatePacklistAPI(packlist, map);
+    packlistUpdated(packlist);
+  }
+
+  Future<bool> highlightPacklist(
+      Packlist packlist, PacklistNotifier packlistNotifier) async {
+    print('highlight packlist begun');
+    if (packlist.endorsedHighlighted) {
+      await updatePacklistAPI(packlist, {'endorsedHighlighted': false});
+      print('packlist highlighted set to false');
+      //highlight(event, false);
+      await getPacklistAPI(packlist.id, packlistNotifier);
+      return true;
+    } else {
+      await updatePacklistAPI(packlist, {'endorsedHighlighted': true});
+      print('packlist highlighted set to true');
+      //highlight(event, true);
+      await getPacklistAPI(packlist.id, packlistNotifier);
+      return true;
+    }
+  }
+
 /* Might be relevant in future
   Future<List<Map<String, dynamic>>> getEventsByDate(DateTime date) async {
     var snaps = await calendarEvents
@@ -81,36 +93,4 @@ class PacklistService {
       yield participants;
     }
   }*/
-
-  Future<void> deletePacklist(Packlist packlist) async {
-    await deletePacklistAPI(packlist);
-  }
-
-  Future<void> updatePacklist(Packlist packlist, Map<String, dynamic> map,
-      Function packlistUpdated) async {
-    await updatePacklistAPI(packlist, map);
-    packlistUpdated(packlist);
-  }
-
-  Future<bool> highlightPacklist(
-      Packlist packlist, PacklistNotifier packlistNotifier) async {
-    print('highlight packlist begun');
-    if (packlist.endorsedHighlighted) {
-      await updatePacklistAPI(packlist, {'endorsedHighlighted': false});
-      print('packlist highlighted set to false');
-      //highlight(event, false);
-      await getPacklistAPI(packlist.id, packlistNotifier);
-      return true;
-    } else {
-      await updatePacklistAPI(packlist, {'endorsedHighlighted': true});
-      print('packlist highlighted set to true');
-      //highlight(event, true);
-      await getPacklistAPI(packlist.id, packlistNotifier);
-      return true;
-    }
-  }
-
-  String getDocumentRef(String packlistID) {
-    return packlists.doc(packlistID).toString();
-  }
 }
