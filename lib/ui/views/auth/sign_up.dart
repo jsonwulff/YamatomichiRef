22a36afda7fa5dbc5c5db7b1c7a9de2076312aee
Ellen-Sofie Frydenlund Krintel Christensen <elch@itu.dk>
2021-04-29 11:06:41 +0200

    Signup looks like figma

--- lib/ui/views/auth/sign_up.dart
+++ lib/ui/views/auth/sign_up.dart
@@ -4,6 +4,7 @@ import 'package:app/middleware/firebase/email_verification.dart';
 import 'package:app/ui/routes/routes.dart';
 import 'package:app/ui/shared/buttons/button.dart';
 import 'package:app/ui/shared/form_fields/text_form_field_generator.dart';
+import 'package:app/ui/shared/navigation/app_bar_custom.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/gestures.dart';
@@ -54,7 +55,6 @@ class SignUpViewState extends State<SignUpView> {
       emailController,
       AuthenticationValidation.validateEmail,
       texts.email,
-      iconData: Icons.email,
       key: Key('SignUp_EmailFormField'),
     );
 
@@ -63,7 +63,6 @@ class SignUpViewState extends State<SignUpView> {
       AuthenticationValidation.validatePassword,
       texts.password,
       autovalidateMode: AutovalidateMode.onUserInteraction,
-      iconData: Icons.lock,
       isTextObscured: true,
       key: Key('SignUp_PasswordFormField'),
     );
@@ -72,7 +71,6 @@ class SignUpViewState extends State<SignUpView> {
       confirmationPasswordController,
       AuthenticationValidation.validateConfirmationPassword,
       texts.confirmPassword,
-      iconData: Icons.lock,
       isTextObscured: true,
       optionalController: passwordController,
       key: Key('SignUp_ConfirmPasswordFormField'),
@@ -129,18 +127,7 @@ class SignUpViewState extends State<SignUpView> {
     }
 
     return Scaffold(
-      appBar: AppBar(
-          title: Text(texts.signUpCAP, style: TextStyle(color: Colors.black)),
-          backgroundColor: Colors.white,
-          brightness: Brightness.dark,
-          leading: new IconButton(
-            icon: new Icon(
-              Icons.arrow_back,
-            ),
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-          )),
+      appBar: AppBarCustom.basicAppBarWithContext(texts.signUpCAP, context),
       body: SafeArea(
         minimum: const EdgeInsets.all(16),
         child: Center(
@@ -152,22 +139,21 @@ class SignUpViewState extends State<SignUpView> {
                 children: [
                   _buildAppLogoImage(),
                   Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Flexible(
-                          child: Padding(
-                        padding: const EdgeInsets.fromLTRB(0, 0, 8, 0),
-                        child: firstNameField,
-                      )),
-                      Flexible(
-                        child: Padding(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Flexible(
+                            child: Padding(
                           padding: const EdgeInsets.fromLTRB(8, 0, 0, 0),
-                          child: lastNameField,
-                        ),
-                      )
-                    ],
-                  ),
+                          child: firstNameField,
+                        )),
+                        Flexible(
+                          child: Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 0, 8, 0),
+                            child: lastNameField,
+                          ),
+                        )
+                      ]),
                   Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: emailField,
@@ -235,6 +221,7 @@ class SignUpViewState extends State<SignUpView> {
                   Padding(
                     padding: const EdgeInsets.only(top: 40.0),
                     child: Button(
+                      width: 200,
                       label: texts.signUp,
                       onPressed: trySignUpUser,
                       key: Key('SignUp_SignUpButton'),
