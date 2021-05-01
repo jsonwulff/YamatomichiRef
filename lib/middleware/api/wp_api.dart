5a3c6d449385db26f29897ca0d243bc9a5d1cc2e
Julian Wulff <julianwulff@gmail.com>
2021-05-01 20:30:13 +0200

    Saved linked to article

--- lib/middleware/api/wp_api.dart
+++ lib/middleware/api/wp_api.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
+// https://hackernoon.com/7-steps-to-build-an-android-and-ios-app-for-your-wordpress-blog-via-flutter-2o4w336p
 Future<List> fetchWpNews() async {
   final response = await http.get(
       Uri.parse("https://flutternerd.com/index.php/wp-json/wp/v2/posts"),
