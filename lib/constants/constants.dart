8bc10e1ae48999a3fa7bc0711fdf72fae5340c22
Lukas Vinther Offenberg Larsen <lukas98@live.dk>
2021-05-02 08:30:31 +0200

    Started translating

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,9 +1,39 @@
 import 'package:flutter/material.dart';
 
+const Map<String, Map<String, List<String>>> coutryRegions = {
+  'English' : countryRegionsEnglish,
+  'Japanese' : countryRegionsJapanese,
+};
+
+const Map<String, List<String>> countryList = {
+  'Egnlish': countriesListEnglish,
+  'Japanese' : countriesListJapanese,
+};
+
+const Map<String, List<String>> genderList = {
+  'Egnlish' : gendersListEnglish,
+  'Japanese' : gendersListJapanese,
+};
+
 const List<Color> profileImageColors = [Colors.blue, Colors.green, Colors.red];
-const List<String> gendersList = ['Male', 'Female', 'Other', 'Prefer not to disclose'];
-const List<String> countriesList = ['Japan', 'Taiwan', 'Hong Kong'];
-const Map<String, List<String>> countryRegions = {
+
+const List<String> gendersListEnglish = [
+  'Male',
+  'Female',
+  'Other',
+  'Prefer not to disclose'
+];
+const List<String> gendersListJapanese = [
+  '男性',
+  '女性',
+  'その他',
+  '公開したくない'
+];
+
+const List<String> countriesListEnglish = ['Japan', 'Taiwan', 'Hong Kong'];
+const List<String> countriesListJapanese = ['日本', '台湾', '香港'];
+
+const Map<String, List<String>> countryRegionsEnglish = {
   'Japan': [
     'Hokkaido',
     'Tohoku',
@@ -17,6 +47,48 @@ const Map<String, List<String>> countryRegions = {
   ],
   'Taiwan': [
     'Northern Taiwan',
+    'Central Taiwan',
+    'Southern Taiwan',
+    'Eastern Taiwan',
+    'Outer islands',
+    'Other',
+  ],
+  'Hong Kong': [
+    'Islands',
+    'Kwai Tsing',
+    'North',
+    'Sai Kung',
+    'Sha Tin',
+    'Tai Po',
+    'Tsuen Wan',
+    'Tuen Mun',
+    'Yuen Long',
+    'Kowloon City',
+    'Kwun Tong',
+    'Sham Shui Po',
+    'Wong Tai Sin',
+    'Yau Tsim Mong',
+    'Central and Western',
+    'Eastern',
+    'Southern',
+    'Wan Chai',
+    'Other',
+  ],
+};
+const Map<String, List<String>> countryRegionsJapanese = {
+  '日本': [
+    '北海道',
+    '東北',
+    '関東地方',
+    '中部地方',
+    '関西地方',
+    '中国地方',
+    '四国',
+    '沖縄を含む九州',
+    'その他',
+  ],
+  '台湾': [
+    '台湾北部',
     'Central Taiwan',
     'Southern Taiwan',
     'Eastern Taiwan',
