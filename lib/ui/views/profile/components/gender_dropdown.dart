026fec7fd66cb6a72ae2b21a3d0bd6683d22e87e
Julian Wulff <julianwulff@gmail.com>
2021-05-04 14:15:25 +0200

    Cleaned a bit

--- lib/ui/views/profile/components/gender_dropdown.dart
+++ lib/ui/views/profile/components/gender_dropdown.dart
@@ -5,34 +5,36 @@ import 'package:flutter/material.dart';
 class GenderDropDown extends StatelessWidget {
   const GenderDropDown({
     Key key,
-    @required this.context,
     @required this.userProfile,
   }) : super(key: key);
 
-  final BuildContext context;
   final UserProfile userProfile;
 
   @override
   Widget build(BuildContext context) {
-    return DropdownButtonFormField(
-      hint: Text('Please select your gender'),
-      onSaved: (String value) {
-        userProfile.gender = value;
-      },
-      validator: (value) {
-        if (value == null) {
-          return 'Please provide your gender';
-        }
-        return null;
-      },
-      value: userProfile.gender, // Intial value
-      onChanged: (value) {},
-      items: gendersList.map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(value),
-        );
-      }).toList(),
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: DropdownButtonFormField(
+        hint: Text('Please select your gender'),
+        onSaved: (String value) {
+          userProfile.gender = value;
+        },
+        // TODO: Insert form field validator here
+        validator: (value) {
+          if (value == null) {
+            return 'Please provide your gender';
+          }
+          return null;
+        },
+        value: userProfile.gender, // Intial value
+        onChanged: (value) {},
+        items: gendersList.map<DropdownMenuItem<String>>((String value) {
+          return DropdownMenuItem<String>(
+            value: value,
+            child: Text(value),
+          );
+        }).toList(),
+      ),
     );
   }
 }
