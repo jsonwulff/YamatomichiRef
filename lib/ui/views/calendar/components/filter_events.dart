97bb12c8fa2cb0b8d6916e9244410227c81107ea
Malou <elmelund@hotmail.dk>
2021-05-09 10:50:24 +0200

    started filtering spots and days

--- lib/ui/views/calendar/components/filter_events.dart
+++ lib/ui/views/calendar/components/filter_events.dart
@@ -1,19 +1,34 @@
 import 'package:app/middleware/notifiers/event_filter_notifier.dart';
+import 'package:flutter/material.dart';
 
 filterEvents(List<Map<String, dynamic>> events, EventFilterNotifier eventFilterNotifier) {
   if (eventFilterNotifier == null) return;
+  RangeValues _currentOpenSpotsValues = eventFilterNotifier.currentOpenSpotsValues;
+  RangeValues _currentDaysValues = eventFilterNotifier.currentDaysValues;
+  String _country = eventFilterNotifier.country;
+  String _region = eventFilterNotifier.region;
+  bool _showMeGeneratedEvents = eventFilterNotifier.showMeGeneratedEvents;
+  bool _showUserGeneratedEvents = eventFilterNotifier.showUserGeneratedEvents;
+  bool _showYamaGeneratedEvents = eventFilterNotifier.showYamaGeneratedEvents;
+  List<bool> _selectedCategories = eventFilterNotifier.selectedCategories;
 
   //Filter spots
-  events = events.where((event) {
-    int openSpots = event['maxParticipants'] - event['participants'].length;
-    if (openSpots < eventFilterNotifier.currentOpenSpotsValues.start ||
-        openSpots > eventFilterNotifier.currentOpenSpotsValues.end)
-      return false;
-    else
+  if (_currentOpenSpotsValues != null)
+    events = events.where((event) {
+      int openSpots = event['maxParticipants'] - event['participants'].length;
+      if (openSpots < _currentOpenSpotsValues.start || openSpots > _currentOpenSpotsValues.end)
+        return false;
       return true;
-  });
-  //Filter days
+    });
 
+  //Filter days
+  if (_currentDaysValues != null)
+    events = events.where((event) {
+      int days = DateTime.parse(event['endDate']).compareTo(DateTime.parse(event['startDate']));
+      print(days);
+      if (days > _currentDaysValues.start || days < _currentDaysValues.end) return false;
+      return true;
+    });
   //Filter Country
 
   //Filter region
@@ -22,12 +37,3 @@ filterEvents(List<Map<String, dynamic>> events, EventFilterNotifier eventFilterN
 
   //Filter categories
 }
-
-// RangeValues _currentOpenSpotsValues;
-// RangeValues _currentDaysValues;
-// String _country;
-// String _region;
-// bool _showMeGeneratedEvents;
-// bool _showUserGeneratedEvents;
-// bool _showYamaGeneratedEvents;
-// List<bool> _selectedCategories;
